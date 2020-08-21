c7a49db03f83d5a4a61234d6c7821b2e126fa9c7
kv <gxthrj@163.com>
2020-08-21 16:29:59 +0800

    fix: bad indentation

--- example/gen-config-yaml.sh
+++ example/gen-config-yaml.sh
@@ -5,8 +5,8 @@ export etcd_url='http://172.18.5.10:2379'
 wget https://raw.githubusercontent.com/apache/apisix/master/conf/config-default.yaml -O config.yaml
 
 if [[ "$unamestr" == 'Darwin' ]]; then
-   sed -i '' -e ':a' -e 'N' -e '$!ba' -e "s/allow_admin[a-z: #\/._]*\n\( *- [0-9a-zA-Z: #\/._',]*\n*\)*//g" config.yaml
-   sed -i '' -e "s%http://[0-9.]*:2379%`echo $etcd_url`%g" config.yaml
+	sed -i '' -e ':a' -e 'N' -e '$!ba' -e "s/allow_admin[a-z: #\/._]*\n\( *- [0-9a-zA-Z: #\/._',]*\n*\)*//g" config.yaml
+	sed -i '' -e "s%http://[0-9.]*:2379%`echo $etcd_url`%g" config.yaml
 else
 	sed -i -e ':a' -e 'N' -e '$!ba' -e "s/allow_admin[a-z: #\/._]*\n\( *- [0-9a-zA-Z: #\/._',]*\n*\)*//g" config.yaml
 	sed -i -e "s%http://[0-9.]*:2379%`echo $etcd_url`%g" config.yaml
