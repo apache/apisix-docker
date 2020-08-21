687171ff601391fbb9bdd91060ce03b9f5a5d5e6
kv <gxthrj@163.com>
2020-08-21 16:28:40 +0800

    fix: wget -O

--- example/gen-config-yaml.sh
+++ example/gen-config-yaml.sh
@@ -2,7 +2,7 @@
 
 export etcd_url='http://172.18.5.10:2379'
 
-wget https://raw.githubusercontent.com/apache/apisix/master/conf/config-default.yaml -o config.yaml
+wget https://raw.githubusercontent.com/apache/apisix/master/conf/config-default.yaml -O config.yaml
 
 if [[ "$unamestr" == 'Darwin' ]]; then
    sed -i '' -e ':a' -e 'N' -e '$!ba' -e "s/allow_admin[a-z: #\/._]*\n\( *- [0-9a-zA-Z: #\/._',]*\n*\)*//g" config.yaml
