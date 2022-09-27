fb3ad703ddf4d98e461a4dc34ea8a2e6475cdeeb
soulbird <zhaothree@gmail.com>
2022-09-27 21:59:00 +0800

    fix: stand-alone mode unavailable

--- centos/docker-entrypoint.sh
+++ centos/docker-entrypoint.sh
@@ -25,7 +25,10 @@ if [[ "$1" == "docker-start" ]]; then
         cat > ${PREFIX}/conf/config.yaml << _EOC_
 apisix:
   enable_admin: false
-  config_center: yaml
+deployment:
+  role: data_plane
+  role_data_plane:
+    config_provider: yaml
 _EOC_
 
         cat > ${PREFIX}/conf/apisix.yaml << _EOC_
