1b8e04e07db9a6bec8c37575f638819eb1713246
Ashish Tiwari <ashishjaitiwari15112000@gmail.com>
2023-09-25 11:43:23 +0530

    fix: readd the apisix.yaml in entrypoint script
    Signed-off-by: Ashish Tiwari <ashishjaitiwari15112000@gmail.com>

--- debian-dev/docker-entrypoint.sh
+++ debian-dev/docker-entrypoint.sh
@@ -23,15 +23,19 @@ PREFIX=${APISIX_PREFIX:=/usr/local/apisix}
 if [[ "$1" == "docker-start" ]]; then
     if [ "$APISIX_STAND_ALONE" = "true" ]; then
       # If the file is not present then initialise the content otherwise update relevant keys for standalone mode
+      if [ ! -f "${PREFIX}/conf/apisix.yaml" ]; then
+        cat > ${PREFIX}/conf/apisix.yaml << _EOC_
+routes:
+  -
+#END
+_EOC_
+      fi
       if [ ! -f "${PREFIX}/conf/config.yaml" ]; then
           cat > ${PREFIX}/conf/config.yaml << _EOC_
 deployment:
   role: data_plane
   role_data_plane:
     config_provider: yaml
-routes:
-  -
-#END
 _EOC_
       else
           wget -qO /usr/local/apisix/yq https://github.com/mikefarah/yq/releases/latest/download/yq_linux_amd64 \
